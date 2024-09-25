d089950ff58f08bd5760344a5783084e43a2e5a0
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-09-25 07:22:19 -0300

    feat: create buy button

--- app/(main)/home/_components/welcome.tsx
+++ app/(main)/home/_components/welcome.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export const Welcome = () => {
     return (
-        <div className="w-full h-screen flex px-5">
-            <h1 className="text-default 2xl:text-lg">Olá, <span className="font-medium text-light-green">Pedro Silva</span>! Aqui você pode visualizar e informar seus exames.</h1>
+        <div className="w-full h-screen px-5">
+            <div className="flex flex-col gap-y-3 sm:flex-row justify-between items-center">
+                <h1 className="text-default 2xl:text-lg">Olá, <span className="font-medium text-light-green">Pedro Silva</span>! Aqui você pode visualizar e informar seus exames.</h1>
+                <Button variant="green" className="w-[50%] sm:w-[150px]">Contratar um plano</Button>
+            </div>
         </div>
     )
 }
