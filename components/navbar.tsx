40b7666417c91c28035e7a16b8a1caa5b108307d
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-09-26 19:58:12 -0300

    fix: logo redirect

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,11 @@
+import { useRouter } from "next/navigation"
 import Image from "next/image"
 
 export const Navbar = () => {
+    const router = useRouter()
     return (
         <div className="w-full flex items-center justify-between px-5 py-3 border-b border-gray-500 bg-slate-50">
-            <Image src="/logoBlack.png" width={65} height={65} alt="logo" />
+            <Image src="/logoBlack.png" className="cursor-pointer" width={65} height={65} alt="logo" onClick={() => router.push('/')} />
             <div className="h-10 w-10 rounded-full bg-light-green border border-gray-500" />
         </div>
     )
